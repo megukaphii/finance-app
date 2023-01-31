6f1ce78c0b23fccaeb6f03e52126fa89d8ab022f
Megukaphii <blaze5565@gmail.com>
2023-01-31 19:43:57 +1100

    Implemented Parser Class
    -Moved parsing DB output responsibility to new Parser class
    -Minor refactors

--- Server/Database/Database.cs
+++ Server/Database/Database.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Data.Sqlite;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System.Data;
+using System.Reflection;
 
 namespace Server.Database;
 
@@ -16,72 +15,21 @@ public class Database : IDisposable {
 		return this;
 	}
 
-	public List<T> ExecuteReader<T>(string sql, Parameters vars) where T : new() /* Generally better design for model classes */ {
+	public List<T> ExecuteReader<T>(string sql, Parameters vars) where T : new() {
 		var command = DB.CreateCommand();
 		command.CommandText = sql;
 		command.Parameters.AddRange(vars.ToSqliteParameterEnumerable());
 
 		List<T> result = new();
 
-		using (var reader = command.ExecuteReader()) {
-			/*// Field count is the no. of columns, not rows? Though, hm. I guess we need a 2D array or something?
-			// GetValues() obtains the column values of the current row. It's parameter is an object array of values
-			// to be copied into.
-			// Ah, yep, got it.
-			// Hm, so, do we use GetValues and parse, or GetFieldType and GetFieldValue<T>? Probably the latter?
-			// The latter does sound better, yeah hahahahah*/
-			var type = typeof(T);
-			var properties = type.GetProperties(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance);
-
-			while (reader.Read()) {
-				var t = new T();
-
-				for (int i = 0; i < reader.FieldCount; i++) {
-					Type fieldType = reader.GetFieldType(i);
-
-					/*// How use Type?
-					// But, isn't T in this context, the object, i.e. Transaction, while GetFieldValue<T> wants a T
-					// Right, I forgot. We need a model class.
-					// Oh yeah, one of those. What are they?
-					// Basically a class with public getter and setter properties. Those properties are to be written to by this
-					// reader and read from by the user.
-					// Now, time for the usual reflection BS.
-					// Oh dear God.
-					// Hhahahahahah lol
-
-					// Generally we only want public properties. We can tweak filter parameters with enum values as we see fit later.
-					// ...also cos I can't be bothered thinking about it lol
-					// Aha, fair enough.
-					// Ah, I've placed it inside the loop. Whoops.
-
-					// I feel like yours is a better way of doing this...
-					// Maybe, but my brain melted in the process thinking about a couple of edge cases LOL
-					// So your brain survived this hahahahahahaha
-					// Oh dear, was it truly worth it for the code to look pretty?
-					// On par for the course ;)*/
-
-					var fieldName = reader.GetName(i);
-					var field = properties.SingleOrDefault(p => p.Name == fieldName);
-
-					// TODO: To throw or not to throw if the local class type does not contain property
-					// corresponding to the incoming data from the database? Your call, Cory.
-					if (field != null) {
-						field.SetValue(t, reader.GetValue(i)); // TODO: Async
-					} else {
-						// Throw here if you want to throw when property does not exist in class
-						throw new Exception($"{type.Name} doesn't contain field {fieldName}");
-					}
-				}
-				result.Add(t);
-			}
+		using (SqliteDataReader reader = command.ExecuteReader()) {
+			Type type = typeof(T);
+			PropertyInfo[] properties = type.GetProperties();
+			Parser parser = new Parser(reader, type, properties);
+			result = parser.PerformParse<T>();
 		}
 
 		return result;
-	}
-
-	private T ParseObject<T>() where T : new() {
-		// TODO: lil' refactor
-		return new T();
 	}
 
 	public Database ExecuteNonQuery(string sql, Parameters vars) {
---
+++ Server/Database/Parser.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Reflection;
+
+namespace Server.Database;
+internal class Parser {
+	private readonly SqliteDataReader reader;
+	private readonly Type parseInto;
+	private readonly PropertyInfo[] properties;
+
+	public Parser(SqliteDataReader reader, Type parseInto, PropertyInfo[] properties) {
+		this.reader = reader;
+		this.parseInto = parseInto;
+		this.properties = properties;
+	}
+
+	public List<T> PerformParse<T>() where T : new() {
+		List<T> result = new();
+
+		while (reader.Read()) {
+			result.Add(ParseObject<T>());
+		}
+
+		return result;
+	}
+
+	private T ParseObject<T>() where T : new() {
+		T t = new();
+		for (int i = 0; i < reader.FieldCount; i++) {
+			Type fieldType = reader.GetFieldType(i);
+
+			string fieldName = reader.GetName(i);
+			PropertyInfo? field = properties.SingleOrDefault(p => p.Name == fieldName);
+
+			if (field != null) {
+				field.SetValue(t, reader.GetValue(i)); // TODO: Async
+			} else {
+				throw new Exception($"{parseInto.Name} doesn't contain field {fieldName}");
+			}
+		}
+		return t;
+	}
+}
