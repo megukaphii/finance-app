80cea06480d5200359cd3896fc0bded58ee3d7b2
Megukaphii <40730335+Megukaphii@users.noreply.github.com>
2023-06-02 22:00:05 +1000

    Update Tests
    -Separate Create test into Create and Select
    -Add Update test

--- FinanceApp.ServerTests/DatabaseTest.cs
+++ FinanceApp.ServerTests/DatabaseTest.cs
@@ -7,6 +7,7 @@ namespace ServerTest;
 [TestClass]
 public class TransactionTest {
 	const long EXODIA_THE_FORBIDDEN_ONE = 120;
+	const long OTHER_YUGIOH_REFERENCE = 150;
 
 	[ClassInitialize]
 	public static void ClassInit(TestContext context) {
@@ -17,11 +18,37 @@ public class TransactionTest {
 	[TestMethod]
 	public void TestCreateTransaction() {
 		using (SqliteDatabase db = new()) {
-			Transaction test = new Transaction(db, EXODIA_THE_FORBIDDEN_ONE).Save();
+			Transaction test = new(db, EXODIA_THE_FORBIDDEN_ONE);
 
-			Transaction? saved = Transaction.Find(1);
+			Assert.AreEqual(0, test.ID);
 
-			Assert.AreEqual(test, saved);
+			test.Save();
+
+			Assert.AreEqual(1, test.ID);
+		}
+	}
+
+	[TestMethod]
+	public void TestSelectTransaction() {
+		using (SqliteDatabase db = new()) {
+			Transaction expected = new(db, EXODIA_THE_FORBIDDEN_ONE);
+			expected.ID = 1;
+
+			Transaction? result = Transaction.Find(1);
+
+			Assert.AreEqual(expected, result);
+		}
+	}
+
+	[TestMethod]
+	public void TestUpdateTransaction() {
+		using (SqliteDatabase db = new()) {
+			Transaction transaction = Transaction.Find(1)!;
+			transaction.Value = OTHER_YUGIOH_REFERENCE;
+
+			transaction.Save(); // transaction.Update(); // transaction.Update(Value, OTHER_YUGIOH_REFERENCE); ?
+
+			Assert.AreEqual(1, transaction.Value);
 		}
 	}
 }
